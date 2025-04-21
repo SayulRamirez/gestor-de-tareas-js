c6e1e07212047e197062dddcdfa75fc01b86ceee
SayulRamirez <sayulramirez@gmail.com>
2025-04-20 19:27:03 -0600

    feature: agregado el archivo para realizar las validaciones

---
+++ js/validations.js
@@ -0,0 +1,39 @@
+const limit = {
+    nombre: { max: 50, min: 1 },
+    apellido: { max: 30, min: 1 },
+    matero: { max: 30, min: 0 },
+    telefono: { max: 15, min: 10 },
+};
+
+function validSize(...campos) {
+    let errores = [];
+
+    campos.forEach(({ nombre, valor }) => {
+        const limite = limit[nombre]; 
+        if (!limite) return;
+
+        if (valor.trim().length < limite.min || valor.trim().length > limite.max) {
+            errores.push(`El campo ${nombre} debe tener entre ${limite.min} y ${limite.max} caracteres.`);
+        }
+    });
+
+    return errores;
+}
+
+function containText(element) {
+    if (element) {
+        let values = Object.values(element);
+        
+        for (let i = 0; i < values.length; i++) {
+            if (values[i] === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+function emailValid(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
