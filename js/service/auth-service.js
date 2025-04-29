e59b4e6790105664c885b6c24140ef842c05d48c
SayulRamirez <sayulramirez@gmail.com>
2025-04-28 21:16:06 -0600

    feature: agregado el servicio para el login del usuario

--- js/service/auth-service.js
+++ js/service/auth-service.js
@@ -1,7 +1,7 @@
 async function registerUser(user) {
     try {
         const response = await axios.post("http://localhost:8080/auth/register", user);
-        return { code: response.status, data: response.data }; // ✅ Si el registro es exitoso
+        return { code: response.status, data: response.data };
     } catch (error) {
         if (error.response) {
             return {
@@ -9,6 +9,21 @@ async function registerUser(user) {
                 code: error.response.status
             };
         }
-        return { message: "Error de conexión", code: null };
     }
+    return { message: "Error de conexión", code: null };
+}
+
+async function login(credentials) {
+    try {
+        const response = await axios.post("http://localhost:8080/auth/login", credentials);
+        return { code: response.status, data: response.data };
+    } catch (error) {
+        if (error) {
+            return { message: error.response.data.message, 
+                code: error.response.status,
+            };
+        }
+
+    }
+    return { message: "Error de conexión", code: null};
 }
