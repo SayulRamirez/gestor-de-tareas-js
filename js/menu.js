7ff099200adaf1d4692472b54633c1b04f0d92d0
SayulRamirez <sayulramirez@gmail.com>
2025-06-25 18:35:29 -0600

    refactor: se eliminaron algunos archivos y se juntaron el contenido con otros relacionados

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-// Script para el menu
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
